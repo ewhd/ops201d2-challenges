54030a2459ac0238b16e2c4809a626e6af05be51
ewhd <ewhd22@gmail.com>
2021-01-27 07:29:35 +0000

    I think this works

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Script Name:                  helloworld
+# Class Name:                   Ops 201
+# Author Name:                  Ethan Denny
+# Date of latest revision:      7/26/2020
+# Purpose:                      fetch network adapter information and print it to output.exe
+
+# What is a variable?
+net_adapter_info=true
+
+# Declaration
+net_adapter_info=$(lspci)
+
+# Call
+echo $net_adapter_info > output.txt
