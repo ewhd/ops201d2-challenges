443a6a76640b3a852baaa29210f1749d1425ba8d
ewhd <ewhd22@gmail.com>
2021-02-02 08:21:05 +0000

    This program now fulfills its task (not stretch goals)

--- challenge06-netcat.sh
+++ challenge06-netcat.sh
@@ -7,9 +7,8 @@
 #               Call for a target IP address from the user
 #               A function prints all currently listening, well known ports of target computer
 #               
+# 
 
-# Scan localhost for port 22
-nc -z -v 127.0.0.1 22
 
 # Notes:
 # "well known ports" means ports 0-1023
@@ -20,28 +19,23 @@ nc -z -v 127.0.0.1 22
 ### This variable contains the IPv4 address of the target computer
 target_ip=""
 
-### This array contains the port number from 0-1023 on which the computer with IPv4 
-### address $target_ip is listening
-listening_ports=()
+
 
 # Declare Functions
 
-### This function prints all currently listening ports between 0-1023 on the computer 
-### with IPv4 address $target_ip and appends them to the array listening_ports
-### It calls target_ip as an argument
+### This function finds all currently listening ports from 1-1023 on a computer and prints them
+### It's first argument is the IPv4 address of the target computer
 port_scan_fun() {
-
-
-
+    nc -z -v $1 1-1023 2>&1 | grep succeeded
 }
 
 # Main
-
+echo "test2"
 ### This line gets a target IP from the user
-read "Enter IPv4 address of target computer to scan for listening ports from 0-1023: " target_ip
+read -p "Enter IPv4 address of target computer to scan for listening ports from 1-1023: " target_ip
 
-### This line calls the function port_scan_fun() 
-port_scan_fun target_ip
+### This line calls the function port_scan_fun() with the variable target_ip as its argument
+port_scan_fun $target_ip 
 
 
 # End
